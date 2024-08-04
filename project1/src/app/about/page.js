2318e339de564642f1515a002d9c4b2a1b4a269d
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-08-04 15:32:22 +0530

    Metadata handling

--- project1/src/app/about/page.js
+++
@@ -1,3 +0,0 @@
-export default function About(){
-    return <h1>We are a new firm</h1>
-}
---
+++ project1/src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export const metadata ={
+    title: "About page"
+}
+
+export default function About(){
+    return (<h1>We are a new firm</h1>);
+}
