814da6c9239533754dcd0584cc8c36839e83bb70
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-08-05 22:50:37 +0530

    Error handling types

--- project1/src/app/products/[productId]/page.tsx
+++ project1/src/app/products/[productId]/page.tsx
@@ -1,4 +1,9 @@
+"use client";
 import {Metadata} from "next";
+
+function getRandomInt(count: number){
+    return Math.floor(Math.random()*count);
+}
 
 type Props = {
     params:{
@@ -17,6 +22,12 @@ export const generateMetadata = ({params}:Props):Metadata => {
 export default function Product({params} : {
     params : {productId : string};
 }){
+    const random = getRandomInt(2);
+
+    if(random===1){
+        throw new Error("Random to One Nikla XD");
+    }
+
     return (
         <h1>This is product {params.productId}</h1>
     );
