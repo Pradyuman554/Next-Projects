f8fe77d855c41c34a04b35ab06f1715f62241b2d
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-08-05 16:55:08 +0530

    Error Boundary

---
+++ project1/src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function ErrorBoundary() {
+    return (
+        <div>
+            Error Occured. Sorry Pal
+        </div>
+    );
+}
--- project1/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ project1/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,19 +1,25 @@
-import { notFound } from "next/navigation"
+import { notFound } from "next/navigation";
 
-export default function Review({params}:
-    {
-        params : {
-            reviewId : string
-            productId : string
-        };
-    }
-)
-{   
-    if (parseInt(params.reviewId) < 1000){
-        notFound();
-    }
+export default function Review({
+  params,
+}: {
+  params: {
+    reviewId: string;
+    productId: string;
+  };
+}) {
+  if (parseInt(params.reviewId) < 1000) {
+    notFound();
+  }
 
-    return (
-        <h1>This is the review {params.reviewId} for product {params.productId}</h1>
-    )
+  const number = 1;
+  if (number === 1) {
+    throw new Error("Dabba Gol");
+  }
+
+  return (
+    <h1>
+      This is the review {params.reviewId} for product {params.productId}
+    </h1>
+  );
 }
