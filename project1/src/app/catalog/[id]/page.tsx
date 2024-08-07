0cbd0e6e6f91a82db99ab6c1ace2d0bf0aece258
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-08-08 01:39:47 +0530

    Modal

---
+++ project1/src/app/catalog/[id]/page.tsx
@@ -0,0 +1,21 @@
+import items, {info} from "../index";
+import Image from "next/image";
+
+
+export default function Page({params : {id},} : {
+    params : {id : string};
+}) {
+
+    const photo : info = items.find((p)=>p.id==id)!;
+
+    return (
+        <div>
+            <h1 className="font-black text-4xl">{photo.name}</h1>
+            <Image
+                alt={photo.name}
+                src={photo.src}
+            />
+            <p>Color of the attire: {photo.color}</p>
+        </div>
+    );
+}
