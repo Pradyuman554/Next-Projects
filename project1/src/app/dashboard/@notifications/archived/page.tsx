8b7c153484b6e0316d2b2429a15ad563e07441dc
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-08-07 22:28:35 +0530

    Healed Slot (parellel routing)

--- project1/src/app/dashboard/@notifications/archived/page.tsx
+++ project1/src/app/dashboard/@notifications/archived/page.tsx
@@ -1,11 +1,14 @@
-// app/dashboard/archived/page.tsx
-
 import Card from "@/app/components/Card";
+import Link from "next/link";
 
 export default function ArchivedPage() {
     return (
+        <>
         <Card>
             This is the Archived page.
         </Card>
+        <br/>
+        <Link href="/dashboard">Go back</Link>
+        </>
     );
 }
