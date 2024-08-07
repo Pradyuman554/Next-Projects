92efaa30000fbc504d12d0f96fc94033985b298b
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-08-07 22:29:59 +0530

    Default.tsx

--- project1/src/app/dashboard/@notifications/default.tsx
+++ project1/src/app/dashboard/@notifications/default.tsx
@@ -1,4 +1,4 @@
-export default function defaultdashboard() {
+export default function defaultnotifications() {
     return (
         <div className="font-black text-xl text-blue-500">
             This is the normal page.
