4255b5fc94bdf183dac2a36c63c1620bc58819ab
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-08-08 23:42:05 +0530

    patch request

--- project2/src/app/comments/[id]/route.ts
+++ project2/src/app/comments/[id]/route.ts
@@ -9,3 +9,14 @@ export async function GET(
 
   return Response.json(comment);
 }
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const { name } = body;
+  const index = data.findIndex((com) => com.id === params.id);
+  data[index].name = name;
+  return Response.json(data[index]);
+}
