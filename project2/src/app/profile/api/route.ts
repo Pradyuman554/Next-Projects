4bbb4e1377077484571cc3f74afd47d70cd9529a
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-08-09 21:03:09 +0530

    Update route.ts

--- project2/src/app/profile/api/route.ts
+++ project2/src/app/profile/api/route.ts
@@ -1,13 +1,16 @@
 import { NextRequest } from "next/server";
 import { headers } from "next/headers";
 
-export async function GET() {
+export async function GET(request : NextRequest) {
   const headerlist = headers();
 
-  console.log(headerlist.get("Authorization"));
+    const theme = request.cookies.get("theme");
+
+  console.log(headerlist.get("Authorization"),theme);
   return new Response("<h1>Done</h1>", {
     headers: {
         "Content-type":"text/html",
+        "Set-Cookie":"theme=dark",
     },
   });
 }
