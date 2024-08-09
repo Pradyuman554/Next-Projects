ccee6c18a45a172a97c3bbfff01170d8c748f88e
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-08-09 20:53:05 +0530

    Update route.ts

--- project2/src/app/profile/api/route.ts
+++ project2/src/app/profile/api/route.ts
@@ -1,9 +1,13 @@
 import { NextRequest } from "next/server";
 import { headers } from "next/headers";
 
-export async function GET(){
-    const header = headers();
+export async function GET() {
+  const headerlist = headers();
 
-    console.log(header.get("Authorization"));
-    return new Response("Done");
+  console.log(headerlist.get("Authorization"));
+  return new Response("<h1>Done</h1>", {
+    headers: {
+        "Content-type":"text/html",
+    },
+  });
 }
