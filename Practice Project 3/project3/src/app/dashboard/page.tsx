40c0142aa5e7b3004d666a70967e9f20a14472f4
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-08-11 19:27:30 +0530

    Dynamic Rendering

--- Practice Project 3/project3/src/app/dashboard/page.tsx
+++ Practice Project 3/project3/src/app/dashboard/page.tsx
@@ -1,14 +1,15 @@
-"use client";
+// "use client";
 import { useState } from "react";
 
 export default function Dashboard() {
-    const [name, setName] = useState("");
+    // const [name, setName] = useState("");
     
     return (
         <div className="bg-white m-5">
             Dashboard Page
-            <input className="border border-black ml-2" value={name} onChange={(e)=>setName(e.target.value)}></input>
-            <p>Hello {name}</p>
+            {/* <input className="border border-black ml-2" value={name} onChange={(e)=>setName(e.target.value)}></input> */}
+            {/* <p>Hello {name}</p> */}
+            <h1>The time is {new Date().toLocaleTimeString()}</h1>
         </div>
     );
 }
