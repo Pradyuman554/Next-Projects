550f152b1697f7e1217d2d28fbc31ba54428a9f8
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-08-11 12:33:48 +0530

    ok

--- project2/src/app/profile/api/route.ts
+++ project2/src/app/profile/api/route.ts
@@ -3,16 +3,18 @@ import { headers, cookies } from "next/headers";
 
 export async function GET(request : NextRequest) {
     //Give header to client and set their browsers cookies
-    const theme = request.cookies.get("theme");
     cookies().set("Sasrikal","Paaji");
 
     //Get headers from client
-    const headerlist = headers();
-  console.log(headerlist.get("Authorization"),theme);
-  return new Response("<h1>Done</h1>", {
-    headers: {
-        "Content-type":"text/html",
-        "Set-Cookie":"theme=dark",
-    },
-  });
+  //   const headerlist = headers();
+  //   const theme = request.cookies.get("theme");
+
+  //   console.log(headerlist.get("Authorization"),theme);
+  // return new Response("<h1>Done</h1>", {
+  //   headers: {
+  //       "Content-type":"text/html",
+  //       "Set-Cookie":"theme=dark",
+  //   },
+  // }
+//);
 }
--- project2/src/middleware.ts
+++ project2/src/middleware.ts
@@ -1,12 +1,14 @@
 import {NextResponse, type NextRequest} from 'next/server';
 
-export function middleware(request: NextRequest) {
-    // return NextResponse.redirect(new URL("/", request.url));
-    if(request.nextUrl.pathname === "/profile")
-        // return NextResponse.redirect(new URL("/hello", request.url));
-        return NextResponse.rewrite(new URL("/hello", request.url));
-}
+export function middleware(request: NextRequest){
+    const response = NextResponse.next();
+    
+    const theme = request.cookies.get("theme");
+    if(!theme){
+        response.cookies.set("theme", "dark");
+    }
 
-// export const config = {
-    // matcher: "/profile",
-// };
+    response.headers.set("Hello", "Namaste");
+
+    return response;
+}
