1879a1c9f7174a6b1831f8ed9f03684f0630343c
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-08-11 10:57:47 +0530

    Update middleware.ts

--- project2/src/middleware.ts
+++ project2/src/middleware.ts
@@ -1,9 +1,12 @@
 import {NextResponse, type NextRequest} from 'next/server';
 
 export function middleware(request: NextRequest) {
-    return NextResponse.redirect(new URL("/", request.url));
+    // return NextResponse.redirect(new URL("/", request.url));
+    if(request.nextUrl.pathname === "/profile")
+        // return NextResponse.redirect(new URL("/hello", request.url));
+        return NextResponse.rewrite(new URL("/hello", request.url));
 }
 
-export const config = {
-    matcher: "/profile",
-};
+// export const config = {
+    // matcher: "/profile",
+// };
