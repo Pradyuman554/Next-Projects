9303d64f2482e689a9eec2140a6b7b6783fb7347
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-08-11 19:24:56 +0530

    First commit and client side rendering and client component

---
+++ Practice Project 3/project3/src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { useState } from "react";
+
+export default function Dashboard() {
+    const [name, setName] = useState("");
+    
+    return (
+        <div className="bg-white m-5">
+            Dashboard Page
+            <input className="border border-black ml-2" value={name} onChange={(e)=>setName(e.target.value)}></input>
+            <p>Hello {name}</p>
+        </div>
+    );
+}
